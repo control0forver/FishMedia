4b03cb51ce7764777faa6887672d227eedf01fd1
To ling <sjzbjs@outlook.com>
2023-04-09 02:46:54 +0800

    添加项目文件。

---
+++ FishMedia/FishMediaConfig.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FishMedia
+{
+    public class Node<T, TIndex>
+    {
+        public T Data { get; set; }
+        public Dictionary<TIndex, Node<T, TIndex>> Next { get; set; }
+        //public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+        public Node()
+        {
+            Data = default(T);
+            Next = null;
+        }
+    }
+    public class FishMediaConfigItem : Dictionary<string, string>
+    {
+        public FishMediaConfigItem() { }
+        public FishMediaConfigItem(string key, string value) { base[key] = value; }
+    };
+    public class FishMediaConfigNode : Node<FishMediaConfigItem, string> { };
+
+    public class FishMediaConfig
+    {
+        public const string c_strDefaultConfig =
+            "# Default Config\n" +
+            "[Config]\n" +
+            "  [Servers]\n" +
+            "    [Web]\n" +
+            "      RootDir=www\n" +
+            "      Index=index.htm\n" +
+            "\n" +
+            "      IpAddr=127.0.0.1\n" +
+            "      Port=8080\n" +
+            "      IpAddr6=::1\n" +
+            "      Port6=8080\n" +
+            "    END\n" +
+            "\n" +
+            "    [Rtmp]\n" +
+            "    END\n" +
+            "  END\n" +
+            "END\n" +
+            "";
+        public const string c_strDefaultConfigPath = "config.conf";
+        public static Encoder c_encDefaultEncoder = Encoding.ASCII.GetEncoder();
+        public static Decoder c_decDefaultDecoder = Encoding.ASCII.GetDecoder();
+
+        public FishMediaConfigNode nodeConfigNodeTree { get; private set; } = new FishMediaConfigNode();
+        public string strConfigData { get; private set; } = c_strDefaultConfig;
+        public string strConfigPath { get; private set; } = c_strDefaultConfigPath;
+        public Encoder encEncoder { get; private set; } = c_encDefaultEncoder;
+        public Decoder decDecoder { get; private set; } = c_decDefaultDecoder;
+
+        public FishMediaConfig(bool bResolveWithDefault = false)
+        {
+            if (bResolveWithDefault)
+                ResolveConfig();
+        }
+
+        public static FishMediaConfig NewConfigByData(string strConfigData)
+        {
+            FishMediaConfig config = new FishMediaConfig();
+
+            config.strConfigData = strConfigData;
+
+            return config;
+        }
+
+        public static FishMediaConfig NewConfigByFile(string strConfigPath)
+        {
+            FishMediaConfig config = new FishMediaConfig();
+            config.LoadConfigFromFile(strConfigPath);
+
+            return config;
+        }
+
+        unsafe public void LoadConfig(bool bResolveConfig = true)
+        {
+            LoadConfigFromFile(strConfigPath, bResolveConfig);
+        }
+
+        unsafe public void LoadConfigFromFile(string strConfigPath, bool bResolveConfig = true)
+        {
+            if (CanFileAccess(strConfigPath, true, true, true))
+            {
+                byte[] arr_byteBytes = File.ReadAllBytes(strConfigPath);
+                int iCharCount = decDecoder.GetCharCount(arr_byteBytes, 0, arr_byteBytes.Length);
+
+                if (iCharCount > 0)
+                {
+                    char[] strDecodedData = new char[iCharCount];
+                    fixed (byte* p_arr_byteBytes = arr_byteBytes)
+                    {
+                        fixed (char* p_strDecodedData = strDecodedData)
+                        {
+                            decDecoder.GetChars(p_arr_byteBytes, arr_byteBytes.Length, p_strDecodedData, iCharCount, true);
+                        }
+                    }
+                    decDecoder.GetChars(arr_byteBytes, strDecodedData, true);
+
+                    strConfigData = new string(strDecodedData);
+                }
+
+                if (bResolveConfig)
+                    ResolveConfig();
+            }
+        }
+
+        public void LoadConfigFromMemory(string strConfigData, bool bResolveConfig = true)
+        {
+            this.strConfigData = strConfigData;
+
+            if (bResolveConfig)
+                ResolveConfig();
+        }
+
+        public void ResolveConfig()
+        {
+            string strConfigDataCopy = this.strConfigData;
+
+            bool bNewLineHasEnter = false;
+            bNewLineHasEnter = strConfigDataCopy.Contains('\r');
+            string strNewLine = "";
+            if (bNewLineHasEnter)
+                strNewLine = "\r\n";
+            else
+                strNewLine = "\n";
+            string[] arr_strConfigDataLines = strConfigDataCopy.Split(strNewLine);
+
+            if (arr_strConfigDataLines.Length <= 0)
+                return;
+
+            int iLineIndex = 0;
+            void MakeNode(ref FishMediaConfigNode ParentNood)
+            {
+                FishMediaConfigItem NodeTree = new FishMediaConfigItem();
+                Dictionary<string, Node<FishMediaConfigItem, string>> NextNoods = new Dictionary<string, Node<FishMediaConfigItem, string>>();
+
+                while (iLineIndex < arr_strConfigDataLines.Length)
+                {
+                    string strConfigDataLine = arr_strConfigDataLines[iLineIndex].Trim();
+
+                    // Igrone comment
+                    if (strConfigDataLine.Contains('#'))
+                    {
+                        strConfigDataLine = strConfigDataLine.Remove(strConfigDataLine.IndexOf('#'));
+                    }
+
+                    if (strConfigDataLine.Length > 0)
+                    {
+                        if (strConfigDataLine == "END")
+                            break;
+
+                        // Get into node
+                        if (strConfigDataLine.StartsWith('[') && strConfigDataLine.EndsWith(']'))
+                        {
+                            string strNodeKey = strConfigDataLine.Substring(strConfigDataLine.IndexOf('[') + 1, strConfigDataLine.IndexOf(']') - 1 - strConfigDataLine.IndexOf('['));
+                            NodeTree[strNodeKey] = ""; // Add Node
+
+                            FishMediaConfigNode NextNood = new FishMediaConfigNode();
+                            ++iLineIndex;
+                            MakeNode(ref NextNood);
+                            NextNoods[strNodeKey]=NextNood;
+                        }
+                        else
+                        {
+                            string strNodeMemberKey = strConfigDataLine.Substring(0, strConfigDataLine.IndexOf('='));
+                            string strNodeMemberValue = strConfigDataLine.Substring(strConfigDataLine.IndexOf('=') + 1, strConfigDataLine.Length - 1 - strNodeMemberKey.Length);
+
+                            NodeTree[strNodeMemberKey] = strNodeMemberValue;
+                        }
+                    }
+
+                    ++iLineIndex;
+                }
+
+                ParentNood.Next = NextNoods;
+                ParentNood.Data = NodeTree;
+            }
+
+            FishMediaConfigNode node = new FishMediaConfigNode();
+            MakeNode(ref node);
+            nodeConfigNodeTree = node;
+
+        }
+
+        public void SaveConfig()
+        {
+            int iByteCount = encEncoder.GetByteCount(strConfigData, true);
+            byte[] arr_byteBytes = new byte[iByteCount];
+            encEncoder.GetBytes(strConfigData, arr_byteBytes, true);
+
+            File.WriteAllBytes(strConfigPath, arr_byteBytes);
+        }
+
+        public void SetPath(string strConfigPath)
+        {
+            this.strConfigPath = strConfigPath;
+        }
+
+        public void SetEncoder(Encoder encEncoder)
+        {
+            this.encEncoder = encEncoder;
+        }
+
+        public void SetDecoder(Decoder decDecoder)
+        {
+            this.decDecoder = decDecoder;
+        }
+
+
+        // Utils
+        private static bool CanFileAccess(string strPath, bool bPermissionNeed = true, bool bTryCreate = false, bool bTryGetPermission = false)
+        {
+            if (!File.Exists(strPath))
+            {
+                if (!bTryCreate)
+                    return false;
+
+                try
+                {
+                    File.Create(strPath).Dispose();
+                }
+                catch (Exception eEx)
+                {
+                    Console.Error.WriteLine(eEx.Message);
+                    return false;
+                }
+            }
+
+            bool bPermissionAccess = false;
+            try
+            {
+                File.Open(strPath, FileMode.Open, FileAccess.ReadWrite).Dispose();
+                bPermissionAccess = true;
+            }
+            catch (Exception eEx)
+            {
+                bPermissionAccess = false;
+            }
+
+
+            if (bPermissionNeed)
+            {
+                if (bPermissionAccess)
+                    return true;
+
+                if (bTryGetPermission)
+                {
+                    try
+                    {
+                        File.SetAttributes(strPath, FileAttributes.Normal | FileAttributes.ReadOnly);
+                    }
+                    catch (Exception eEx)
+                    {
+                        Console.Error.WriteLine(eEx.Message);
+                        return false;
+                    }
+
+                    try
+                    {
+                        File.Open(strPath, FileMode.Open, FileAccess.ReadWrite).Dispose();
+                        return true;
+                    }
+                    catch (IOException)
+                    {
+                        Console.Error.WriteLine("Get Permission Failed!");
+                        return false;
+                    }
+                }
+                else
+                    return false;
+            }
+            else
+                return true;
+
+        }
+    }
+}
---
+++ FishMedia/Program.cs
@@ -0,0 +1,241 @@
+﻿using Servers.HTTP;
+using System.Text;
+
+namespace FishMedia
+{
+    public class WebServer : HttpServer
+    {
+        public string strIndex = "index.htm";
+
+        public WebServer(string strIpAddress, int iPort, string strRoot, string strIndex)
+            : base(strIpAddress, iPort, strRoot)
+        {
+            this.strIndex = strIndex;
+        }
+
+        public override void OnPost(HttpRequest request, HttpResponse response)
+        {
+            string data = request.Params == null ? "" : string.Join(";", request.Params.Select(x => x.Key + "=" + x.Value).ToArray());
+
+            string content = string.Format("Data:{0}", data);
+
+            response.SetContent(content);
+            response.Content_Encoding = "utf-8";
+            response.StatusCode = "200";
+            response.Content_Type = "text/html; charset=UTF-8";
+            response.Headers["Server"] = "HttpServer";
+
+            response.Send();
+        }
+
+        public override void OnGet(HttpRequest request, HttpResponse response)
+        {
+            string requestURL = request.URL;
+            requestURL = requestURL.Replace("/", @"\").Replace("\\..", "").TrimStart('\\');
+            string requestFile = Path.Combine(ServerRoot, requestURL);
+
+            string extension = Path.GetExtension(requestFile);
+
+            if (extension != "")
+            {
+                response = response.FromFile(requestFile);
+            }
+            else
+            {
+                if (Directory.Exists(requestFile) && !File.Exists(requestFile + "\\" + strIndex))
+                {
+                    requestFile = Path.Combine(ServerRoot, requestFile);
+                    var content = ListDirectory(requestFile, requestURL);
+                    response = response.SetContent(content, Encoding.UTF8);
+                    response.Content_Type = "text/html; charset=UTF-8";
+                }
+                else
+                {
+                    requestFile = Path.Combine(requestFile, strIndex);
+                    response = response.FromFile(requestFile);
+                    response.Content_Type = "text/html; charset=UTF-8";
+                }
+            }
+
+            response.Send();
+        }
+
+        public override void OnDefault(HttpRequest request, HttpResponse response)
+        {
+
+        }
+
+        private string ConvertPath(string[] urls)
+        {
+            string html = string.Empty;
+            int length = ServerRoot.Length;
+            foreach (var url in urls)
+            {
+                var s = url.StartsWith("..") ? url : url.Substring(length).TrimEnd('\\');
+                html += String.Format("<li><a href=\"{0}\">{0}</a></li>", s);
+            }
+
+            return html;
+        }
+
+        private string ListDirectory(string requestDirectory, string requestURL)
+        {
+            var folders = requestURL.Length > 1 ? new string[] { "../" } : new string[] { };
+            folders = folders.Concat(Directory.GetDirectories(requestDirectory)).ToArray();
+            var foldersList = ConvertPath(folders);
+
+            var files = Directory.GetFiles(requestDirectory);
+            var filesList = ConvertPath(files);
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(string.Format("<html><head><title>{0}</title></head>", requestDirectory));
+            builder.Append(string.Format("<body><h1>{0}</h1><br/><ul>{1}{2}</ul></body></html>",
+                 requestURL, filesList, foldersList));
+
+            return builder.ToString();
+        }
+    }
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    internal class Program
+    {
+        static WebServer webServer = null;
+        static Thread webServerThread = new Thread(webServerThreadHandler) { IsBackground = true };
+        static void webServerThreadHandler()
+        {
+            webServer.Start();
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Fish Media   Ver. 1.0\n");
+
+            Console.WriteLine("(Press Any Key to Continue)");
+            Console.ReadKey(true);
+
+            FishMediaConfig config = new FishMediaConfig();
+            config.SetPath("FishMedia.conf");
+            config.LoadConfig();
+
+
+            Console.WriteLine("Starting Web Server");
+            {
+                FishMediaConfigNode nodeIndex = config.nodeConfigNodeTree;
+                string RootDir, Index, IpAddr, Port, IpAddr6, Port6;
+
+                void ReadConfig()
+                {
+                    if (nodeIndex.Data.ContainsKey("Config"))
+                    {
+                        nodeIndex = (FishMediaConfigNode)nodeIndex.Next["Config"];
+                        if (nodeIndex.Data.ContainsKey("Servers"))
+                        {
+                            nodeIndex = (FishMediaConfigNode)nodeIndex.Next["Servers"];
+                            if (nodeIndex.Data.ContainsKey("Web"))
+                            {
+                                nodeIndex = (FishMediaConfigNode)nodeIndex.Next["Web"];
+
+                                foreach (var item in nodeIndex.Data)
+                                {
+                                    switch (item.Key)
+                                    {
+                                        default:
+                                            break;
+
+                                        case "RootDir":
+                                            {
+                                                RootDir = item.Value;
+                                                break;
+                                            }
+                                        case "Index":
+                                            {
+                                                Index = item.Value;
+                                                break;
+                                            }
+                                        case "IpAddr":
+                                            {
+                                                IpAddr = item.Value;
+                                                break;
+                                            }
+                                        case "Port":
+                                            {
+                                                Port = item.Value;
+                                                break;
+                                            }
+                                        case "IpAddr6":
+                                            {
+                                                IpAddr6 = item.Value;
+                                                break;
+                                            }
+                                        case "Port6":
+                                            {
+                                                Port6 = item.Value;
+                                                break;
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Read Default Config
+                nodeIndex = new FishMediaConfig(true).nodeConfigNodeTree;
+                ReadConfig();
+
+                // Read Current Config
+                nodeIndex = config.nodeConfigNodeTree;
+                ReadConfig();
+
+                webServer = new WebServer("127.0.0.1", 8080, "www", "index.htm");
+                webServer.Logger = new ConsoleLogger();
+                webServerThread.Start();
+            }
+
+            while (true)
+            {
+                string strCmdLine = "";
+
+                Console.Write(">");
+                strCmdLine = Console.ReadLine().Trim();
+
+                if (strCmdLine == string.Empty)
+                    continue;
+
+                {
+                    if (strCmdLine == "help")
+                    {
+                        Console.WriteLine("Commands:");
+                        Console.WriteLine(" help - Show all commands");
+                        Console.WriteLine(" show - Show current config");
+                        Console.WriteLine(" exit - Exit all servers");
+                    }
+
+                    if (strCmdLine == "show")
+                    {
+                        Console.WriteLine(config.strConfigData);
+                    }
+
+                    if (strCmdLine == "exit")
+                    {
+                        webServer.Stop();
+                        if (webServerThread.IsAlive)
+                            webServerThread.Join();
+
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("[Debug]Program Finished (Press Any Key)");
+            Console.ReadKey(true);
+            return 0;
+        }
+    }
+}
